3d6f433c95803539995cdfdaa4023d3ccfafff38
Kazem Jahanbakhsh <k.jahanbakhsh@gmail.com>
2020-09-07 13:10:34 -0700

    add punctuations

--- src/analyze.py
+++ src/analyze.py
@@ -1,4 +1,11 @@
+import requests
+import pickle
+
+
 def load_transcripts(filename):
+    """
+    Takes the transcript input file, parse and returns it
+    """
     transcripts = []
     with open(filename) as fp:
         transcript_counter, state = 0, 0
@@ -35,6 +42,9 @@ def load_transcripts(filename):
 
 
 def dedup(text):
+    """
+    Removes duplicate phrases from spoken transcripts
+    """
     tokens = text.split()
     tmp_tokens, clean_tokens = [], []
     # get rid of any dupe bigram
@@ -68,6 +78,9 @@ def dedup(text):
 
 
 def remove_fillers(text):
+    """
+    Removes fillers from text
+    """
     # Ref: https://en.wikipedia.org/wiki/Filler_(linguistics)
     fillers = set(["um", "er", "uh"])
     tokens = text.split()
@@ -79,27 +92,127 @@ def remove_fillers(text):
     return " ".join(clean_tokens)
 
 
-def cleanup(transcripts):
+def process(transcripts):
     for idx, transcript in enumerate(transcripts):
-        transcripts[idx]["text"] = dedup(transcript["text"])
+        dedup_text = dedup(transcript["text"])
+        text = remove_fillers(dedup_text)
+        if text:
+            transcripts[idx]["text"] = text
+        else:
+            print(text)
 
 
-def generate_html(transcripts):
-    base_url = "https://youtu.be/iZM_JmZdqCw?t="
+def get_punctuation(text):
+    url = "http://bark.phon.ioc.ee/punctuator"
+    data = {'text': text}
+    r = requests.post(url = url, data = data)
+    return r.text
+
+
+def get_unsegmented_text(transcripts):
+    unsegmented_text, video_start_times = [], []
+    tokens_so_far = 0
+    for idx, transcript in enumerate(transcripts):
+        text = transcript["text"]
+        start_time = str(transcript["start"])  # in second
+        time_slot = transcript["period"].split(" --> ")[0]
+        video_start_times.append((tokens_so_far, start_time, time_slot))
+        unsegmented_text.append(text)
+        # get length of this segment in terms of tokens
+        tokens_so_far += len(text.split())
+    unsegmented_text = " ".join(unsegmented_text)
+    return unsegmented_text, video_start_times
+
+
+def punctuate(unsegmented_text):
+    # print(unsegmented_text)
+    # get the punctuation
+    punctuated_text = get_punctuation(unsegmented_text)
+    return punctuated_text
+
+
+def get_video_slot_times(punctuated_text, video_start_times):
+    sentences = punctuated_text.split('.')
+    start_times = []
+    visited_tokens_so_far = 0
+    for sent in sentences:
+        tokens_no = len(sent.split())
+        print("sent: {}, tokens no: {}".format(sent, tokens_no))
+        prev_tokens_so_far, next_tokens_so_far = 0, 0
+        for idx, item in enumerate(video_start_times):
+            next_tokens_so_far, start_time, time_slot = item
+            print("idx: {}, prev_tokens_so_far: {}, next_tokens_so_far: {}, start_time: {}".\
+                format(idx, prev_tokens_so_far, next_tokens_so_far, start_time))
+            if next_tokens_so_far >= visited_tokens_so_far:
+                print("found --> idx: {}, prev_tokens_so_far: {}, next_tokens_so_far: {}, visited_tokens_so_far: {}, start_time: {}".\
+                    format(idx, prev_tokens_so_far, next_tokens_so_far, visited_tokens_so_far, start_time))
+                break
+            prev_tokens_so_far = next_tokens_so_far
+        try:
+            # check if visited tokens so far is closer to prev_tokens_so_far or next_tokens_so_far
+            dist_from_prev = abs(prev_tokens_so_far - visited_tokens_so_far)
+            dist_from_next = abs(next_tokens_so_far - visited_tokens_so_far)
+            print("sentence: {}".format(sent))
+            if dist_from_prev < dist_from_next:
+                start_time, time_slot = video_start_times[idx-1][1], video_start_times[idx-1][2]
+                print("visited tokens so far: {} is closer to prev_tokens_so_far: {}! start_time: {}".\
+                    format(visited_tokens_so_far, prev_tokens_so_far, start_time))
+            else:
+                start_time, time_slot = item[1], item[2]
+                print("visited tokens so far: {} is closer to next_tokens_so_far: {}! start_time: {}".\
+                    format(visited_tokens_so_far, next_tokens_so_far, start_time))
+            start_times.append((start_time, time_slot))
+        except IndexError:
+            continue
+        visited_tokens_so_far += tokens_no
+    return start_times
+
+
+
+def generate_html(transcripts, base_url):
     html = ""
     for idx, transcript in enumerate(transcripts):
-        start = transcript["start"]
-        dedup_text = dedup(transcript["text"])
-        transcripts[idx]["text"] = remove_fillers(dedup_text)
-        text = transcript["text"]
         start_time = transcript["period"].split(" --> ")[0]
-        url = base_url + str(start)
-        html += '<p><span><a href="' + url + '" target="_blank">[' + start_time + ']</a> – </span>' + text + '</p>\n'
+        url = base_url + str(transcript["start"])
+        html += '<p><span><a href="' + url + '" target="_blank">[' + start_time + ']</a> – </span>' + transcript["text"] + '</p>\n'
     return html
 
 
-filename = "data/peterthiel_you_are_not_a_lottery_ticket_2013_sxsw.srt"
-transcripts = load_transcripts(filename)
-html = generate_html(transcripts)
-with open("peter_theil.html", "w") as fp:
-    fp.write(html)
+def generate_html(punctuated_text, start_times, base_url):
+    html = ""
+    sentences = punctuated_text.split('.')
+    trans_inx = 0
+    for idx, sentence in enumerate(sentences):
+        try:
+            start_time, period = start_times[idx]
+        except IndexError:
+            continue
+        url = base_url + start_time
+        html += '<p><span><a href="' + url + '" target="_blank">[' + period + ']</a> – </span>' + sentence + '.' + '</p>\n'
+    return html
+
+
+def run():
+    filename = "data/peterthiel_you_are_not_a_lottery_ticket_2013_sxsw.srt"
+    yt_base_url = "https://youtu.be/iZM_JmZdqCw?t="
+    transcripts = load_transcripts(filename)
+    # dedupe text & remove fillers
+    process(transcripts)
+    unsegmented_text, video_start_times = get_unsegmented_text(transcripts)
+    # print(video_start_times)
+    # punctuated_text = punctuate(transcripts)
+    # with open("data/punctuated.txt", "wb") as fp:
+    #     pickle.dump(punctuated_text, fp)
+    with open('data/punctuated.txt', 'rb') as fp:
+        punctuated_text = pickle.load(fp)
+    # print(punctuated_text)
+    start_times = get_video_slot_times(punctuated_text, video_start_times)
+    print(len(start_times))
+    print(len(punctuated_text.split('.')))
+    html = generate_html(punctuated_text, start_times, yt_base_url)
+    # html = generate_html(transcripts, punctuated_text, yt_base_url)
+    with open("peter_theil_with_punc.html", "w") as fp:
+        fp.write(html)
+
+
+run()
